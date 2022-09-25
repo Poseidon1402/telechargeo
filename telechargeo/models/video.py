b2104e63b639ed0a5b16272db05f710ea06b0ae6
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-09-25 18:09:45 +0300

    add explicit type

--- telechargeo/models/video.py
+++ telechargeo/models/video.py
@@ -1,10 +1,9 @@
 class Video :
 
-    def __init__(self, artist, title) -> None:
+    def __init__(self, artist: str, title: str) -> None:
         self._artist = artist
         self._title = title
     
     def getFullDescription(self) -> str:
 
         return '{0} - {1}'.format(self._artist, self._title)
-    
