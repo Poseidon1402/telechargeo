37ecf2b57bc5cd0bea49ed87dece2b28bd8cfbad
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-09-27 21:10:01 +0300

    download successfull after code refactoring

--- telechargeo/__main__.py
+++ telechargeo/__main__.py
@@ -1,6 +1,7 @@
 from youtubesearchpython import VideosSearch
 from telechargeo.handlers.audioHandler import AudioHandler
 from telechargeo.models.video import Video
+from telechargeo.views.dataDisplayer import ResultDisplayer
 
 if __name__ == "__main__":
     artist = input("Ny anaran'ilay artiste: ")
@@ -8,7 +9,11 @@ if __name__ == "__main__":
     video = Video(artist, title)
 
     videosSearch = VideosSearch(video.getFullDescription(), limit = 5)
-    handler = AudioHandler(videosSearch.result()['result'][0]['link'])
+    videoDisplayer = ResultDisplayer(videosSearch.result()['result'])
+    videoDisplayer.displayResultOfTheSearch()
+    
+    choosed = int(input('Which will you choose ? '))
+    handler = AudioHandler(videosSearch.result()['result'][choosed-1]['link'])
     handler.displayAllFetchedAudio()
     choose = int(input('You choose: '))
     handler.downloadingTheChoosedAudio(choose)
